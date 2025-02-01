3bb1945ecdf250e8eacf3da31d24a611729a10db
silassequeira <silassequeiraa@gmail.com>
2025-02-01 13:44:06 +0000

    fixed video not working, adjusted the function for responsivity

--- scripts/moveButtons.js
+++ scripts/moveButtons.js
@@ -5,15 +5,17 @@ const moveButton = (button, targetMobile, targetDesktop, isMobile) => {
 };
 
 const moveButtons = () => {
-    const [button1, button2] = document.querySelectorAll('.button');
-    const [infoFlex1, infoFlex2] = document.querySelectorAll('.infoFlex');
+    const [button1, button2, button3] = document.querySelectorAll('.button');
+    const [infoFlex1, infoFlex2, infoFlex3] = document.querySelectorAll('.infoFlex');
     const imgContainer = document.querySelector('.imgContainer');
     const projectsGrid = document.querySelector('.layoutGrid');
+    const video = document.querySelector('.video');
     const isMobile = window.innerWidth < 995;
 
     if (button1 && button2 && infoFlex1 && infoFlex2) {
         moveButton(button1, imgContainer, infoFlex1, isMobile);
-        moveButton(button2, projectsGrid, infoFlex2, isMobile);
+        moveButton(button3, projectsGrid, infoFlex3, isMobile);
+        moveButton(button2, video, infoFlex2, isMobile);
     }
 };
 
